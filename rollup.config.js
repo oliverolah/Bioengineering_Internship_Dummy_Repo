3f2aef47d54278b97f58a39a6f72d266212088a0
oliverOlah <oliverolah13@gmail.com>
2021-11-03 10:47:23 +0100

    another dependencies and config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import babel from 'rollup-plugin-babel';
+import npm from "rollup-plugin-node-resolve";
 
 export default {
    input: 'js/main.js',
@@ -11,6 +12,9 @@ export default {
    plugins: [
       babel({
          exclude: 'node_modules/**',
+      }),
+      npm({
+         jsnext: true
       }),
    ],
 };
